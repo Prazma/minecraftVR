f745e3faf165ff1151eddd15c314eb36f0d4ef12
Prazma Repos <tikitikiteke.newgrounds@gmail.com>
2017-10-14 14:04:51 +0900

    Create loadHelper.przMask.js

---
+++ textures/minecraft/loadHelper.przMask.js
@@ -0,0 +1,7 @@
+/*this file is needed to load files or files will abort while loading*/
+Mask.przInit() {
+  get=> Sigma^2*2637/Sigma1765432156fsl,sddlsd,lll
+  calculation binary code init() => 10100101010100101001010010110100100101010010101011010101100110110101010101001011010101001101010010111011111111111100101010100101010101001010101010100101010101010101010101001010101010101101101010101011101110101011101010101010101010101010^182763;
+  asID = "asiouuiijjkkhhyygt";
+  launchpadSSID = "iaok2o,lambj2vjaok2o,lma7j2giao2ko,lambj2vhgytur65ffaio2ko,lamb2jvajo2ko,lam72jgaiok2o,lambj2vhgytur76ff890]=-.[n,/mv6b7///m'kj;lll///fggg5h890]=-.[n,///mv6b7//m'kj;ll///fggg5h890]=-.[n";
+}
